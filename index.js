166b4386363d81a5c37c4a21c94dc5b78655e462
qingsong.wang <1962628113@qq.com>
2019-03-28 19:14:24 +0800

    'input添加原生事件’‘

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-// function Test() {
-//     alert('元素失去焦点')
-// }
-function change(val) {
-    alert("元素的值改变，新的值为"+val)
+function getFocus() {
+    console.log('元素得到焦点')
 }
-function test() {
-   console.log(111111)
+function loseFocus() {
+    console.log('元素失去焦点')
 }
-function aa() {
-    alert('文本被选中')
+function changeValue(val) {
+    console.log("元素的值改变，新的值为"+val)
 }
-function down() {
-    alert("按下按键")
+function Entry() {
+    console.log('正在输入')
 }
-function mm() {
-    alert('鼠标移到元素上')
+function selectText() {
+    console.log('文本被选中')
+}
+function downKey() {
+    console.log("按下按键")
+}
+function clickKey() {
+    console.log("按下按键")
+}
+function mouseIn() {
+    console.log('鼠标移到元素上')
+}
+function mouseout() {
+    console.log('鼠标移出元素上')
+}
+function mousedown() {
+    console.log('鼠标在元素上按下')
+}
+function mouseover() {
+    console.log('鼠标移到元素上')
+}
+function mouseup() {
+    console.log('鼠标在元素上按下后松开')
 }
