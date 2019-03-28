29eee090605a55010d00518d858afc00974ff9e1
qingsong.wang <1962628113@qq.com>
2019-03-28 09:54:49 +0800

    '添加事件‘

---
+++ index.js
@@ -0,0 +1,18 @@
+// function Test() {
+//     alert('元素失去焦点')
+// }
+function change(val) {
+    alert("元素的值改变，新的值为"+val)
+}
+function test() {
+   console.log(111111)
+}
+function aa() {
+    alert('文本被选中')
+}
+function down() {
+    alert("按下按键")
+}
+function mm() {
+    alert('鼠标移到元素上')
+}
